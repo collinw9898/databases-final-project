6b7982cc49e33a9025b58b71e8c3c096c67366c5
Collin Werner <collinw9898@gmail.com>
2018-11-29 20:08:25 -0500

    basic files

---
+++ delete.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html> 
+<body >
+<div style="height:900px; background-color: lightblue;" align="center">
+<br><br><br><br>
+
+
+<p style="color:blue;font-weight:bold;"> write your code here that takes an instructor's ID as input and deletes <br>
+him/her from the instructor table. Please make sure you display <br>
+instructor table before and after the deletion </p>
+
+
+
+<br><br><br><br>
+
+<hr width="50">
+<a href="Frontpage.html" style="color:red;font-weight:bold;">Home</a>
+<hr width="50">
+</div>
+</body> </html>
---
+++ update.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html> 
+<body >
+<div style="height:900px; background-color: lightblue;" align="center">
+<br><br><br><br>
+
+
+<p style="color:blue;font-weight:bold;"> write your code here that takes an instructor's ID as input and updates <br>
+his/her salary by 10%. Please make sure you display <br>
+instructor table before and after the update </p>
+
+<br><br><br><br>
+<hr width="50">
+<a href="Frontpage.html" style="color:red;font-weight:bold;">Home</a>
+<hr width="50">
+</div>
+</body> </html>
