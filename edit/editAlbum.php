19f5ca9e9fdb58ca00b989d7dfa99e8f9c195db3
Collin Werner <collinw9898@gmail.com>
2018-12-05 18:53:18 -0500

    added editAlbum

---
+++ edit/editAlbum.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <div style="height:900px; background-color: lightblue;" align="center">
+            <br><br><br><br>
+
+            <?php
+                require("../tableshow.php");
+                require("../dbconnect.php");
+
+                if(isset($_POST['update'])) {
+                    $a_ID = $_POST['a_ID'];
+                    $a_Name = $_POST['a_Name'];
+
+                    $sql = "UPDATE album ".
+                            "SET album_name = '$a_Name' ".
+                            "WHERE album_id = '$a_ID'";
+
+                    $retval = mysqli_query($conn, $sql);
+
+                    if(!$retval) {
+                        die('Could not update data: ' . mysqli_error($conn));
+                    }
+
+                    echo "Data updated successfully\n";
+
+                    show_albums($conn);
+
+                    mysqli_close($conn);
+                }
+
+                else if(isset($_POST['delete'])) {
+                    $a_ID = $_POST['a_ID'];
+
+                    $sql = "DELETE FROM artist ".
+                            "WHERE artist_id = '$a_ID'";
+
+                    $retval = mysqli_query($conn, $sql);
+
+                    if(!$retval) {
+                        die('Could not delete data: ' . mysqli_error($conn));
+                    }
+
+                    echo "Data deleted successfully\n";
+
+                    show_albums($conn);
+
+                    mysqli_close($conn);
+                }
+
+                else {
+            ?>
+
+            <p>Please enter the ID of the instructor to update</p>
+            <form method="post" action="<?php $_PHP_SELF ?>">
+                <table width = "600" border = "0" cellspacing = "1" cellpadding = "2">
+                    <tr>
+                        <td width = "250">Album ID</td>
+                        <td>
+                            <input name ="a_ID" type ="text" id ="a_ID">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td width = "250">New Album Name</td>
+                        <td>
+                            <input name ="a_Name" type ="text" id ="a_Name">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td width = "250"> </td>
+                        <td>
+                            <input name ="update" type ="submit" id ="update"  value ="Update Album">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td width = "250"> </td>
+                        <td>
+                            <input name ="delete" type ="submit" id ="delete"  value ="Delete Album">
+                        </td>
+                    </tr>
+                </table>
+            </form>
+
+            <?php
+                }
+            ?>
+            <br><br><br><br>
+            <hr width="50">
+            <a href="index.php" style="color:red;font-weight:bold;">Home</a>
+            <hr width="50">
+        </div>
+    </body>
+</html>
--- tableshow.php
+++ tableshow.php
@@ -26,4 +26,30 @@ function show_artists($conn) {
 	}
 }
 
+function show_albums($conn) {
+	$sql = "SELECT album_id, album_name, genre_name FROM artist, genre WHERE artist.genre_id = genre.genre_id";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+		echo '<table border>';
+		echo '<thead><tr>';
+		echo '<th>'."ID".'</th>'.'<th>'."Name".'</th>'.'<th>'."Record Label".'</th>';
+		echo '</tr></thead>';
+		echo '<tbody>';
+
+		while($row = $result->fetch_assoc()) {
+			echo '<tr>';
+			echo "<td>" . $row["album_id"]. "</td>";
+			echo "<td>" . $row["album_name"]. "</td>";
+			echo "<td>" . $row["genre_name"]. "</td>";
+			echo '</tr>';
+		}
+		
+		echo '</tbody>';
+		echo '</table>';
+	} else {
+		echo "0 results";
+	}
+}
+
 ?>
