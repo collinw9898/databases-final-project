d4e2798477a3cd8da74eab44df4f6ebcbf095c52
Collin Werner <collinw9898@gmail.com>
2018-12-05 20:55:12 -0500

    image fix

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <body >
         <div style="background-color: lightblue;" align="center">
             <table>
-                <tr><td><image src="musehub.png" width="150" height="100"></td></tr>
+                <tr><td><image src="musehub.png"></td></tr>
             </table>
 
             <br><br><br><br>
