1e7ea311e3fce3d54af7e491d150d6486161e252
disrupt3d <disrupt3r@hotmail.com>
2018-12-05 21:42:29 -0500

    fixed search, now searches genres, albums, and artists, need songs

--- search.php
+++ search.php
@@ -5,14 +5,17 @@
             <br><br><br><br>
 
             <?php
-                require("../tableshow.php");
-                require("../dbconnect.php");
+                require("tableshow.php");
+                require("dbconnect.php");
 
                 if(isset($_POST['search_album'])) {
-                    $a_ID = $_POST['a_ID'];
+                    $album_name = $_POST['album_name'];
 
-                    $sql = "SELECT album ".
-                            "WHERE album_id = '$a_ID'";
+                    $sql = "SELECT * ".
+                            "FROM album ".
+                            "WHERE album_name = '$album_name'";
+                    
+                    $result = $conn->query($sql);
 
                     $retval = mysqli_query($conn, $sql);
 
@@ -20,18 +23,37 @@
                         die('Could not find album data: ' . mysqli_error($conn));
                     }
 
-                    echo "Successfully searched album data\n";
+                    if ($result->num_rows > 0) {
+		                echo '<table border>';
+		                echo '<thead><tr>';
+		                echo '<th>'."ID".'</th>'.'<th>'."Name".'</th>'.'<th>'."Record Label".'</th>';
+		                echo '</tr></thead>';
+		                echo '<tbody>';
 
-                    show_albums($conn);
-
+		                while($row = $result->fetch_assoc()) {
+			                echo '<tr>';
+			                echo "<td>" . $row["album_id"]. "</td>";
+			                echo "<td>" . $row["album_name"]. "</td>";
+			                echo "<td>" . $row["genre_id"]. "</td>";
+			                echo '</tr>';
+		                }
+		
+		                echo '</tbody>';
+		                echo '</table>';
+	                } else {
+		                echo "0 results";
+                	}
                     mysqli_close($conn);
                 }
 
                 else if(isset($_POST['search_artist'])) {
-                    $a_name = $_POST['a_ID'];
+                    $artist_name = $_POST['artist_name'];
 
-                    $sql = "SELECT artist ".
-                            "WHERE artist_name = '$a_Name'";
+                    $sql = "SELECT * ".
+                            "FROM artist ".
+                            "WHERE artist_name = '$artist_name'";
+
+                    $result = $conn->query($sql);
 
                     $retval = mysqli_query($conn, $sql);
 
@@ -39,9 +61,26 @@
                         die('Could not find artist data: ' . mysqli_error($conn));
                     }
 
-                    echo "Successfully searched artist data\n";
 
-                    show_albums($conn);
+                    if ($result->num_rows > 0) {
+		                echo '<table border>';
+		                echo '<thead><tr>';
+		                echo '<th>'."ID".'</th>'.'<th>'."Name".'</th>';
+		                echo '</tr></thead>';
+		                echo '<tbody>';
+
+		                while($row = $result->fetch_assoc()) {
+			                echo '<tr>';
+			                echo "<td>" . $row["artist_id"]. "</td>";
+			                echo "<td>" . $row["artist_name"]. "</td>";
+			                echo '</tr>';
+		                }
+		
+		                echo '</tbody>';
+		                echo '</table>';
+	                } else {
+		                echo "0 results";
+                	}
 
                     mysqli_close($conn);
                 }
@@ -49,8 +88,11 @@
                 else if(isset($_POST['search_genre'])) {
                     $g_ID = $_POST['g_ID'];
 
-                    $sql = "SELECT artist ".
+                    $sql = "SELECT * ".
+                            "FROM genre ".
                             "WHERE genre_ID = '$g_ID'";
+
+                    $result = $conn->query($sql);     
 
                     $retval = mysqli_query($conn, $sql);
 
@@ -58,9 +100,25 @@
                         die('Could not find genre data: ' . mysqli_error($conn));
                     }
 
-                    echo "Successfully searched genre data\n";
+                    if ($result->num_rows > 0) {
+		                echo '<table border>';
+		                echo '<thead><tr>';
+		                echo '<th>'."Name".'</th>'.'<th>'."Description".'</th>';
+		                echo '</tr></thead>';
+		                echo '<tbody>';
 
-                    show_albums($conn);
+		                while($row = $result->fetch_assoc()) {
+			                echo '<tr>';
+			                echo "<td>" . $row["genre_id"]. "</td>";
+			                echo "<td>" . $row["description"]. "</td>";
+			                echo '</tr>';
+		                }
+		
+		                echo '</tbody>';
+		                echo '</table>';
+	                } else {
+		                echo "0 results";
+                	}
 
                     mysqli_close($conn);
                 }
@@ -72,16 +130,16 @@
             <form method="post" action="<?php $_PHP_SELF ?>">
                 <table width = "600" border = "0" cellspacing = "1" cellpadding = "2">
                     <tr>
-                        <td width = "250">Search Album ID</td>
+                        <td width = "250">Search Album Name</td>
                         <td>
-                            <input name ="a_ID" type ="text" id ="a_ID">
+                            <input name ="album_name" type ="text" id ="album_name">
                         </td>
                     </tr>
 
                     <tr>
                         <td width = "250">Search Arist Name</td>
                         <td>
-                            <input name ="a_Name" type ="text" id ="a_Name">
+                            <input name ="artist_name" type ="text" id ="artist_name">
                         </td>
                     </tr>
 
