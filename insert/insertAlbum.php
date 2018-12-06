f1dbf955833a3d42e756969709e7b14ab837e70d
Collin Werner <collinw9898@gmail.com>
2018-12-06 01:31:51 -0500

    handling album creator too

--- insert/insertAlbum.php
+++ insert/insertAlbum.php
@@ -21,10 +21,17 @@
 
                     $a_Name = $_POST['a_Name'];
                     $genre = $_POST['genre'];
+                    $artist = $_POST['artist'];
                     
                     $sql = "INSERT INTO album".
                             "(album_id, album_name, genre_id)".
                             "VALUES ('$a_ID', '$a_Name', '$genre')";
+
+                    $retval = mysqli_query($conn, $sql);
+
+                    $sql = "INSERT INTO album_creator".
+                            "(album_id, artist_id) ".
+                            "VALUES ('$a_ID', '$artist')";
 
                     $retval = mysqli_query($conn, $sql);
 
@@ -49,6 +56,26 @@
                             <td width = "250">New Album Name</td>
                             <td>
                                 <input name ="a_Name" type ="text" id ="a_Name">
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td width = "250">Artist</td>
+                            <td>
+                                <select name="artist" id="artist">
+                                    <option value=""></option>
+                                    <?php 
+                                        $sql = "SELECT artist_id, artist_name FROM artist";
+
+                                        $result = mysqli_query($conn, $sql);
+
+                                        if ($result->num_rows > 0) {                              
+                                            while($row = $result->fetch_assoc()) {
+                                                echo '<option value="' . $row["artist_id"] . '">' . $row["artist_name"] . '</option>';
+                                            }
+                                        }
+                                    ?>
+                                </select>
                             </td>
                         </tr>
 
