cdc5975b7cb6726828e8e4006617c466c510c5d5
Collin Werner <collinw9898@gmail.com>
2018-12-05 21:39:09 -0500

    style

--- index.php
+++ index.php
@@ -8,14 +8,15 @@
         <div align="center">
             <div class="header"><img src="musehub.png" /></div>
 
-            <br><br><br><br>
-            <table>
-                <tr>
-                    <td><a href="insert.php" style="color:blue;font-weight:bold;">Insert </a> &nbsp&nbsp&nbsp&nbsp;</td>
-                    <td><a href="edit.php" style="color:blue;font-weight:bold;">Edit Information</a> &nbsp&nbsp&nbsp&nbsp;</td>
-                    <td><a href="display.php" style="color:blue;font-weight:bold;">Display</a> &nbsp&nbsp&nbsp&nbsp;</td>
-                </tr>
-            </table>
+            <div class="nav">
+                <table>
+                    <tr>
+                        <td><a href="insert.php" style="color:blue;font-weight:bold;">Insert </a> &nbsp&nbsp&nbsp&nbsp;</td>
+                        <td><a href="edit.php" style="color:blue;font-weight:bold;">Edit Information</a> &nbsp&nbsp&nbsp&nbsp;</td>
+                        <td><a href="display.php" style="color:blue;font-weight:bold;">Display</a> &nbsp&nbsp&nbsp&nbsp;</td>
+                    </tr>
+                </table>
+            </div>  
 
             <?php 
                 require("dbconnect.php");
