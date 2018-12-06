23d0aa50c805c357623bb7c5fdae296c256a9289
Collin Werner <collinw9898@gmail.com>
2018-12-06 01:02:31 -0500

    minor changes

--- tableshow.php
+++ tableshow.php
@@ -85,7 +85,7 @@ function show_songs($conn) {
 	if ($result->num_rows > 0) {
 		echo '<table border>';
 		echo '<thead><tr>';
-		echo '<th>'."Song ID".'</th>'.'<th>'."Song Title".'</th>'.'<th>'."Record Label".'</th>'.'<th>'."Genre".'</th>';
+		echo '<th>'."Song ID".'</th>'.'<th>'."Song Title".'</th>'.'<th>'."Artist".'</th>'.'<th>'."Record Label".'</th>'.'<th>'."Genre".'</th>';
 		echo '</tr></thead>';
 		echo '<tbody>';
 
